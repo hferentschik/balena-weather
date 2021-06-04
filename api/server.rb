0d5000e0d254d69f9a9330bde28035ebc4321a00
Hardy Ferentschik <hardy@ferentschik.de>
2021-06-04 23:02:08 +0200

    chore: Adds Rake and Rubocop to API project
    - Adding some tests

--- api/server.rb
+++ api/server.rb
@@ -3,16 +3,10 @@
 require 'httparty'
 require 'json'
 require 'logger'
-require 'sinatra'
-require "sinatra/namespace"
+require 'sinatra/base'
+require 'sinatra/namespace'
 require 'time'
 require 'tzinfo'
-
-logger = Logger.new($stdout)
-$stdout.sync = true
-
-set :logger, logger
-set :bind, '0.0.0.0'
 
 # Defaults to Northpole
 LATITUDE = ENV.fetch('LATITUDE', 90.00)
@@ -20,39 +14,54 @@ LONGITUDE = ENV.fetch('LONGITUDE', 135.00)
 
 TIMEZONE = ENV.fetch('TIMEZONE', 'GMT')
 
-tz = TZInfo::Timezone.get(TIMEZONE)
+# The main App class
+class App < Sinatra::Base
+  register Sinatra::Namespace
 
-logger.info("LATITUDE: #{LATITUDE}")
-logger.info("LONGITUDE: #{LONGITUDE}")
-logger.info("TIMEZONE: #{TIMEZONE}")
+  set :logger, @logger
+  set :bind, '0.0.0.0'
 
-get '/' do
-  'Welcome to the weather API'
-end
+  def initialize(app = nil)
+    super(app)
 
-namespace '/api/v1' do
-
-  before do
-    content_type 'application/json'
+    @tz = TZInfo::Timezone.get(TIMEZONE)
+    @logger = Logger.new($stdout)
+    $stdout.sync = true
+    @logger.info("LATITUDE: #{LATITUDE}")
+    @logger.info("LONGITUDE: #{LONGITUDE}")
+    @logger.info("TIMEZONE: #{TIMEZONE}")
   end
 
-  get '/sunrise' do
-    today = Date.today
-    data = HTTParty.get("https://api.sunrise-sunset.org/json?lat=#{LATITUDE}&lng=#{LONGITUDE}&date=#{today}").body
-    json = JSON.parse(data)
-    sunrise_gmt = Time.parse(json['results']['sunrise'] + " UTC")
-    sunrise_local = tz.to_local(sunrise_gmt)
-
-    { 'results' => {'sunrise' => sunrise_local.strftime('%k:%M').strip}}.to_json
+  get '/' do
+    'Welcome to the weather API'
   end
 
-  get '/sunset' do
-    today = Date.today
-    data = HTTParty.get("https://api.sunrise-sunset.org/json?lat=#{LATITUDE}&lng=#{LONGITUDE}&date=#{today}").body
-    json = JSON.parse(data)
-    sunset_gmt = Time.parse(json['results']['sunset'] + " UTC")
-    sunset_local = tz.to_local(sunset_gmt)
+  namespace '/api/v1' do
+    before do
+      content_type 'application/json'
+    end
 
-    { 'results' => {'sunset' => sunset_local.strftime('%k:%M').strip}}.to_json
+    get '/sunrise' do
+      today = Date.today
+      data = HTTParty.get("https://api.sunrise-sunset.org/json?lat=#{LATITUDE}&lng=#{LONGITUDE}&date=#{today}").body
+      json = JSON.parse(data)
+      sunrise_gmt = Time.parse("#{json['results']['sunrise']}  UTC")
+      sunrise_local = @tz.to_local(sunrise_gmt)
+
+      { 'results' => { 'sunrise' => sunrise_local.strftime('%k:%M').strip } }.to_json
+    end
+
+    get '/sunset' do
+      today = Date.today
+      data = HTTParty.get("https://api.sunrise-sunset.org/json?lat=#{LATITUDE}&lng=#{LONGITUDE}&date=#{today}").body
+      json = JSON.parse(data)
+      sunset_gmt = Time.parse("#{json['results']['sunset']}  UTC")
+      sunset_local = @tz.to_local(sunset_gmt)
+
+      { 'results' => { 'sunset' => sunset_local.strftime('%k:%M').strip } }.to_json
+    end
   end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $PROGRAM_NAME
 end
